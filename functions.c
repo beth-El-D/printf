42d4ee60010dbd0eeb161d1e2c12f97da8d4659b
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-07-24 23:45:31 -0700

    updated

--- functions.c
+++ functions.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_char - prints a char
@@ -115,7 +113,7 @@ int print_int(va_list types, char buffer[], int flags, int width, int precision,
 	n = convert_size_number(n, size);
 
 	if (n == 0)
-		buffer[i--] = '\0';
+		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
@@ -171,7 +169,7 @@ int print_binary(va_list types, char buffer[], int flags, int width, int precisi
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
-		if (sum || i == 3)
+		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
 
